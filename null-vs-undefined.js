e7ada173a5b9c7fe59ee51d6ebfde212e867a22e
shawonahmed1 <mdshawon356@gmail.com>
2020-08-07 17:28:47 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,6 @@
+#null
+
+// null is a special value meaning "no value". null is a special object because typeof null returns 'object'.
+
+#undefined
+//  undefined means that the variable has not been declared, or has not been given a value.
